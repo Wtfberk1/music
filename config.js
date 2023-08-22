b0f97c26c9b65dbaf705f75b4e81c3f4ae6061dc
Wtfberk1 <142720397+Wtfberk1@users.noreply.github.com>
2023-08-22 15:31:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports = {
-    token: "Botunuzun Tokenini Giriniz",
-    id: "Botunuzun ID'sini Giriniz"
+module.exports = {/}
+    token: "Botunuzun Tokenini Giriniz",MTE0MzQ3NzA3NjM4MTQwNTE5NA.Giup8Y.kD4CDTs_ZbXL2JmIm9t1TYr1cMUiPiUYHP1kBE
+    id: "Botunuzun ID'sini Giriniz"1143477076381405194
 }
